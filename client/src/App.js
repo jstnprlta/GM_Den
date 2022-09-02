f8b4b8dfa4bfe79573cab52d730d035648ee237d
Justin Peralta <jstnprlta90@gmail.com>
2022-09-02 00:20:29 -0500

    origin main

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Layout, Image, Typhography } from "antd";
+import Logo from "./images/mafia-png-transparent-mafia-images-16792.png";
 
 const { Title } = Typography;
 const { Header, Footer } = Layout;
 
 const App = () => {
-    return (
-        <Layout>
-            <Header>
-                <Image width="45" preview="false" src={Logo} />
-            </Header>
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <Header>
+        <Image width="45" preview="false" src={Logo} />
+        &nbsp;
+        <Title>GMDen</Title>
+      </Header>
+    </Layout>
+  );
+};
 
 export default App;
