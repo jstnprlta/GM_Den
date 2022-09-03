69fe7100d4dc2cf38fceb756af8d25ba2c3d3ed0
Justin Peralta <jstnprlta90@gmail.com>
2022-09-03 01:33:32 -0500

    origin main

--- client/src/components/StoryForm/styles.js
+++ client/src/components/StoryForm/styles.js
@@ -3,7 +3,7 @@ const styles = {
         display: "flex",
         justificontent: "center",
         alignItem: "center",
-    };
+    },
     formCard: {
         width: 390,
         position: "fixed",
