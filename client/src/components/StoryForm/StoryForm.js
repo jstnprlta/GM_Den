696c001574ddf664ba6a14af7f6c1a3daa160291
Justin Peralta <jstnprlta90@gmail.com>
2022-09-03 01:17:01 -0500

    origin main

--- client/src/components/StoryForm/StoryForm.js
+++ client/src/components/StoryForm/StoryForm.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import {Card, Form, Input, Typography, Button} from "antd";
+import styles from './styles';
+
+
+const { Title } = Typography;
 
 function StoryForm() {
+  const [form] = Form.useForm();
+
+  const onSubmit = () => {
+
+  };
+
+
   return (
-    <div>
-        <h1>
-            story form
-        </h1>
-    </div>
+    <Card
+      style={styles.formCard}
+      title={
+        <Title level={4} style={styles.formTitle}>
+
+        </Title>
+      }
+    >
+      <Form
+        form={form}
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 16 }}
+        layout="horizontal"
+        size="middle"
+        onFinish={onSubmit}
+      
+      >
+        <Form.Item name="username" label="Username">
+          <Input allowClear />
+        </Form.Item>
+      </Form>
+
+    </Card>
   )
 }
 
